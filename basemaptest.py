c17793757e5147f487e61e5287acd595a73b4feb
Unknown <saketh.kollu@gmail.com>
2018-07-13 16:12:35 -0700

    jsoning

--- basemaptest.py
+++ basemaptest.py
@@ -1,29 +1,52 @@
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
+from datetime import datetime as dt
 from mpl_toolkits.basemap import Basemap
-from matplotlib import pyplot as plt
 
+# load the google location history data
+df_gps = pd.read_json('data/Location History.json')
+print('There are {:,} rows in the location history dataset'.format(len(df_gps)))
 
-def mapTut():
-    m = Basemap(projection='mill', llcrnrlat=20, urcrnrlat=50, \
-                llcrnrlon=-130, urcrnrlon=-60, resolution='c')
-    m.drawcoastlines()
-    m.drawcountries()
-    m.drawstates()
-    m.fillcontinents(color='#04BAE3', lake_color='#FFFFFF')
-    m.drawmapboundary(fill_color='#FFFFFF')
+print(df_gps)
+# parse lat, lon, and timestamp from the dict inside the locations column# parse
+df_gps['lat'] = df_gps['locations'].map(lambda x: x['latitudeE7'])
+df_gps['lon'] = df_gps['locations'].map(lambda x: x['longitudeE7'])
+df_gps['timestamp_ms'] = df_gps['locations'].map(lambda x: x['timestampMs'])
 
-    # Houston, Texas
+# convert lat/lon to decimalized degrees and the timestamp to date-time
+df_gps['lat'] = df_gps['lat'] / 10.**7
+df_gps['lon'] = df_gps['lon'] / 10.**7
+df_gps['timestamp_ms'] = df_gps['timestamp_ms'].astype(float) / 1000
+df_gps['datetime'] = df_gps['timestamp_ms'].map(lambda x: dt.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
+date_range = '{}-{}'.format(df_gps['datetime'].min()[:4], df_gps['datetime'].max()[:4])
 
-    lat, lon = 29.7630556, -95.3630556
-    x, y = m(lon, lat)
-    m.plot(x, y, 'ro')
+# drop columns we don't need, then show a slice of the dataframe
+df_gps = df_gps.drop(labels=['locations', 'timestamp_ms'], axis=1, inplace=False)
+#df_gps[1000:1005]
 
-    lon, lat = -104.237, 40.125  # Location of Boulder
+# define map colors
+land_color = '#f5f5f3'
+water_color = '#cdd2d4'
+coastline_color = '#f5f5f3'
+border_color = '#bbbbbb'
+meridian_color = '#f5f5f3'
+marker_fill_color = '#cc3300'
+marker_edge_color = 'None'
 
-    xpt, ypt = m(lon, lat)
-    m.plot(xpt, ypt, 'go')
+# create the plot
+fig = plt.figure(figsize=(20, 10))
 
-    plt.title("Geo Plotting")
-    plt.show()
+# draw the basemap and its features
+m = Basemap(projection='kav7', lon_0=0, resolution='c', area_thresh=10000)
+m.drawmapboundary(color=border_color, fill_color=water_color)
+m.drawcoastlines(color=coastline_color)
+m.drawcountries(color=border_color)
+m.fillcontinents(color=land_color, lake_color=water_color)
+m.drawparallels(np.arange(-90., 120., 30.), color=meridian_color)
+m.drawmeridians(np.arange(0., 420., 60.), color=meridian_color)
 
+# project the location history points then scatter plot them
+x, y = m(df_gps['lon'].values, df_gps['lat'].values)
+m.scatter(x, y, s=8, color=marker_fill_color, edgecolor=marker_edge_color, alpha=1, zorder=3)
 
-mapTut()
+# show the map
+plt.show()
--- jsonparse.py
+++ jsonparse.py
@@ -1,25 +1,62 @@
 import json
 import datetime
+import platform
+import pandas as pd
+import pprint
 
-import matplotlib
+class MapData:
+
+    global DATAFILE
+    global DATAPOINTS
+    global PARSEDJSON
+    global DATAFRAME
+
+    def __init__(self, JSON):
+        self.DATAFILE = JSON
+        self.PARSEDJSON = self.parseJSON()
+        self.DATAPOINTS = self.getJSONLength()
+        self.extractLatitudeToColumn()
+        self.extractLongitudeToColumn()
+        self.extractMillisToColumn()
+        self.convertMillisToDateToColumn()
+
+       # self.PARSEDJSON = self.PARSEDJSON.drop(labels=['locations'], axis=1, inplace=False)
+
+    def printGlobals(self):
+        try:
+            print("DATAFILE", self.DATAFILE)
+            print("DATAPOINTS", self.DATAPOINTS)
+            print("PARSEDJSON", self.PARSEDJSON)
+        except AttributeError:
+            print("All globals may not be defined...")
+
+    def parseJSON(self):
+        return pd.read_json(self.DATAFILE)
+
+    def getJSONLength(self):
+        return len(self.PARSEDJSON)
+
+    def extractLatitudeToColumn(self):
+        self.PARSEDJSON['latitudes'] = self.PARSEDJSON['locations'].map(lambda x: float(x['latitudeE7'])) / (10.**7)
+
+    def extractLongitudeToColumn(self):
+        self.PARSEDJSON['longitudes'] = self.PARSEDJSON['locations'].map(lambda x: float(x['longitudeE7'])) / (10.**7)
+
+    def extractMillisToColumn(self):
+        self.PARSEDJSON['millis'] = self.PARSEDJSON['locations'].map(lambda x: float(x['timestampMs']))
+
+    def convertMillisToDateToColumn(self):
+        self.PARSEDJSON['date'] = self.PARSEDJSON['millis'].map(lambda x: self.getUTCTime(x))
+
+    def getUTCTime(self, millis):
+        return datetime.datetime.fromtimestamp(float(millis)/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    def getPositionAtTime(self, millis):
+        return self.PARSEDJSON[self.PARSEDJSON['latitudes'].notnull() & (self.PARSEDJSON['millis'] > millis)]
 
 
-from pprint import pprint
 
-with open("data/Location History.json") as f:
-    data = json.load(f)
 
-def getTimeStamp(index):
-    return data["locations"][index]["timestampMs"]
-
-def getUTCTime(millis):
-    return datetime.datetime.fromtimestamp(float(millis)/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
-
-def getCoordinate(index):
-    return [float(data["locations"][index]["longitudeE7"]) / 10000000
-        , float(data["locations"][index]["latitudeE7"])/ 10000000]
-
-DATAPOINTS = len(data["locations"])
-for i in range(0, 10):
-    print(getUTCTime(getTimeStamp(i)))#.locations[0].timestampMs)
-
+if __name__ == '__main__':
+    sakethData = MapData("data/Location History.json")
+    print(sakethData.getPositionAtTime(152858000000))
